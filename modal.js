76e24105e8d92ebfa4afe0c5775da13cd03fcd20
CaioG13 <caiogoulart131@gmail.com>
2026-06-23 14:56:29 -0300

    atualiza arquivos

--- modal.js
+++ modal.js
@@ -23,30 +23,30 @@ function ativarEventosDetalhes() {
 
             const id = botao.dataset.id;
 
-            const produto = buscarProdutoPorId(Id);
+            const produto = buscarProdutoPorId(id);
 
             if (!produto) return;
 
             document.getElementById("modalTitulo").textContent =
-                produto.Nome;
+                produto.nome;
 
             document.getElementById("modalImagem").src =
-                produto.Imagem;
+                produto.imagem;
 
             document.getElementById("modalDescricao").textContent =
-                produto.Descricao;
+                produto.descricao;
 
             document.getElementById("modalPreco").textContent =
-                `R$ ${produto.Preco}`;
+                `R$ ${produto.preco}`;
 
             document.getElementById("modalPeso").textContent =
-                produto.Peso || "-";
+                produto.peso || "-";
 
             document.getElementById("modalTamanho").textContent =
-                produto.Tamanho || "-";
+                produto.tamanho || "-";
 
             document.getElementById("modalTempo").textContent =
-                 produto.TempoImpressao || "-";
+                 produto.tempoImpressao || "-";
 
                  
                 const containerCores =
